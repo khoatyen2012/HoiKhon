fd7bec80e02af8b00bb1f6d162db25d327225409
khoatyen2012 <33951988+khoatyen2012@users.noreply.github.com>
2018-04-17 15:41:39 +0700

    Firebase xong roi nhung chua thanh cong

--- Assets/Scripts/UI/AddQuestion.cs
+++ Assets/Scripts/UI/AddQuestion.cs
@@ -4,6 +4,10 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
 
 public class AddQuestion : MonoBehaviour {
 
@@ -14,22 +18,26 @@ public class AddQuestion : MonoBehaviour {
 
     public tk2dUIItem btnGuiCauHoi;
     public tk2dUIItem btnBack;
-    public GameObject message;
+    
+	public tk2dTextMesh txtThongBao;
 
     string mName = "";
     string mQuestion = "";
     string mTraLoi = "";
     string mGiaiThich = "";
 
+	DatabaseReference mDatabaseRef;
+
 
     public void setData()
     {
+		txtThongBao.text = "";
         doDefault();
     }
 
     public void doDefault()
     {
-        message.SetActive(false);
+		
         txtName.text = "";
         txtQuestion.text = "";
         txtTraLoi.text = "";
@@ -39,16 +47,53 @@ public class AddQuestion : MonoBehaviour {
 
     public void btnGuiCauHoi_OnClick()
     {
+		
         mName = txtName.text;
         mQuestion = txtQuestion.text;
         mTraLoi = txtTraLoi.text;
         mGiaiThich = txtGiaiThich.text;
-        
+
+
+		if(mName.Equals("") ||mQuestion.Equals("")||mTraLoi.Equals("")||mGiaiThich.Equals(""))
+		{
+			
+			txtThongBao.text = "Vui lòng nhập đầy đủ thông tin để gửi câu hỏi !";
+		
+			
+		}else
+		{
+			writeNewUser ();
+
+		}
 
 
         doDefault();
 
     }
+
+	private void writeNewUser()
+	{
+		try{
+			
+			string id = "adam" + GetUniqueIdentifier ();
+		string datetime = DateTime.Now.Day + "n" + DateTime.Now.Month + "t" + DateTime.Now.Year;
+
+			//Debug.Log(id+"--"+datetime+"--"+mQuestion);
+
+		QuestionUD qs = new QuestionUD (id, datetime, mName, mQuestion, mTraLoi, mGiaiThich);
+		string json = JsonUtility.ToJson(qs);
+
+			mDatabaseRef.Child("questions").Child(datetime).Child(id).SetRawJsonValueAsync(json);
+
+			txtThongBao.text = "Gửi câu hỏi thành công. Chân thành cảm ơn !";
+		}catch (Exception ex)
+		{
+			txtThongBao.text = "Kiểm tra lại kết nối mạng !";
+		}
+	}
+
+
+
     public void btnBack_OnClick()
     {
         doDefault();
@@ -63,6 +108,16 @@ public class AddQuestion : MonoBehaviour {
         btnBack.OnClick += btnBack_OnClick;
         btnGuiCauHoi.OnClick += btnGuiCauHoi_OnClick;
 
+		// Set this before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://first-skein-774.firebaseio.com/");
+		mDatabaseRef = FirebaseDatabase.DefaultInstance.RootReference;
+
+	}
+
+	public static string GetUniqueIdentifier()
+	{
+		System.Guid uid = System.Guid.NewGuid();
+		return uid.ToString();
 	}
 	
 	// Update is called once per frame
